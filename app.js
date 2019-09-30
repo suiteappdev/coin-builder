466ae495ebecb5e4ad0d7db165042684c231eb25
a.castro@fastcodelab.com <armitage@andrews-MacBook-Air.local>
2019-09-30 11:33:15 -0500

    latest

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-const CoinImp = require('coin-imp');
- 
-(async () => {
-  const miner = await CoinImp('782448d82b9ed694aa398e4fd9d166cfb6ab7349bbebba6f070a57b30c920f91'); 
-  await miner.start();
- 
-  miner.on('found', () => console.log('Found!'));
-  miner.on('accepted', () => console.log('Accepted!'));
-  miner.on('update', data =>
-    console.log(`
-    Hashes per second: ${data.hashesPerSecond}
-    Total hashes: ${data.totalHashes}
-    Accepted hashes: ${data.acceptedHashes}
-  `)
-  );
- 
-})();
+module.exports = (req, res) => {
+    res.end(`Hello from Node.js on Now 2.0!`);
+};
